8a65137e710408c14eb75c5f9993bbe48acdea9e
lbb-and-a-little-rabbit <915598980@qq.com>
2025-12-17 17:36:47 +0800

    add_stk

--- hw11/t3.cpp
+++ hw11/t3.cpp
@@ -29,3 +29,36 @@ int main(){
         cout << ans << '\n';
     }
 }
+
+/*
+    f2:
+    #include <bits/stdc++.h>
+
+    using namespace std;
+    using ll=long long;
+
+    int main(){
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        int t;
+        cin >> t;
+        while(t--){
+            int n;
+            cin >> n;
+            vector<int> m(n);
+            for(int i=0;i<n;i++){
+                cin >> m[i];
+            }
+            int cnt=0;
+            stack<int> st;
+            for(int i=0;i<n;i++){
+                while(!st.empty()&&st.top()>m[i]) st.pop();
+                if(st.empty()||st.top()<m[i]){
+                    st.push(m[i]);
+                    cnt++;
+                }
+            }
+            cout << cnt << '\n';
+        }
+    }
+*/
